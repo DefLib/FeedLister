4fad22f109ffc8e68fc01115908f222684ebc28f
liwuncle <liwuncle@gmail.com>
2022-07-27 13:21:33 +0900

    簡易API連携機能完成

---
+++ FeedLister/Controller/APIControll.cs
@@ -0,0 +1,37 @@
+﻿using FeedLister.FeedDecoder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FeedLister.Controller
+{
+    class APIControll
+    {
+        internal List<Channel> GetPopChannelList()
+        {
+            StreamReader sr = new StreamReader(new WebClient().OpenRead(@"http://localhost:8000"));
+            string urlsoruse = sr.ReadToEnd().Replace("\"","");
+            string[] urls = urlsoruse.Split(",");
+
+            var lch = new List<Channel>();
+            foreach(string url in urls)
+            {
+                switch (FeedDownloader.CheckFeeds(url))
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        lch.Add(RSS2.ExtractChennelContent(FeedDownloader.GetxmlDoc(url), url));
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return lch;
+        }
+    }
+}
--- FeedLister/View/PopularFeedView.xaml.cs
+++ FeedLister/View/PopularFeedView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FeedLister.Controller;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +18,101 @@ namespace FeedLister.View
     /// </summary>
     public partial class PopularFeedView : Window
     {
+        private string nowSelected;
+
+        private List<Channel> Lch;
+
         public PopularFeedView()
         {
             InitializeComponent();
+            InitializeChannelList();
+        }
+
+        private void InitializeChannelList()
+        {
+            int id = 0;
+            Lch = new APIControll().GetPopChannelList();
+            foreach (Channel ch in Lch)
+            {
+                CreateChannelCard(ch,id++);
+            }
+        }
+
+        private RadioButton CreateChannelCard(Channel ch, int id)
+        {
+            ChannelCard cc = new ChannelCard();
+            cc.SetCardContent(ch.title, ch.description, ch.link);
+            cc.MinWidth = 320;
+            cc.MinHeight = 100;
+
+            RadioButton rb = new RadioButton();
+            rb.Margin = new Thickness(10, 10, 10, 10);
+            rb.MinWidth = 350;
+            rb.MinHeight = 100;
+            rb.Name = "id_" + id;
+            rb.GroupName = "A";
+            rb.Content = cc;
+            rb.Checked += Rb_Checked;
+            rb.Unchecked += Rb_Unchecked;
+
+            ChannelList.Children.Add(rb);
+
+            return rb;
+        }
+
+        private void ChannelAdd(string URL)
+        {
+            if (new ChannelControll().AddChannel(URL))
+            {
+                MessageBox.Show("登録しました");
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("既に登録されているURLです");
+            }
+        }
+
+        private void Rb_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = e.Source as RadioButton;
+            nowSelected = rb.Name.Replace("id_", "");
+        }
+
+        private void Rb_Unchecked(object sender, RoutedEventArgs e)
+        {
+            nowSelected = "";
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string url = Lch[int.Parse(nowSelected.Replace("id_", ""))].feedLink;
+            if (url.Length == 0)
+            {
+                MessageBox.Show("URLを入力してください");
+                return;
+            }
+
+            try
+            {
+                int flag = FeedDownloader.CheckFeeds(url);
+                if (flag == -999)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("無効なURLです");
+                return;
+            }
+
+            ChannelAdd(url);
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
